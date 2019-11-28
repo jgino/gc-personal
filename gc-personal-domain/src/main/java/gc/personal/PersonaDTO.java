7e569715a1e6ca09d7c5c5214a902d90681e8e67
Jonatan <jginmen@gobiernodecanarias.org>
2019-11-28 15:01:06 +0100

    Add initial project

---
+++ gc-personal-domain/src/main/java/gc/personal/PersonaDTO.java
@@ -0,0 +1,15 @@
+package gc.personal;
+
+public class PersonaDTO {
+    private Integer id;
+    private String name;
+    
+    public PersonaDTO() {
+        this.id = null;
+        this.name = null;
+    }
+    
+    
+    
+    
+}
